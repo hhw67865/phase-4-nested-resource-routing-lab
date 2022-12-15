1d1b9989b93e74d97c3058e4883b2c4e84f0c388
Henry Wu <hhw67865@gmail.com>
2022-12-15 12:59:48 -0500

    commit tree 9fdfeb6b29b5735a56fcc7e42cc7b8cb855bd016

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :no_record_error
+
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      render json: User.find(params[:user_id]).items, status: :accepted
+    else
+      items = Item.all
+      render json: items, include: :user, status: :accepted
+    end
   end
 
+  def show
+    if params[:user_id]
+      render json: User.find(params[:user_id]).items.find(params[:id]), status: :accepted
+    else
+      items = Item.find(params[:id])
+      render json: items, include: :user, status: :accepted
+    end
+  end
+
+  def create
+    if params[:user_id]
+      render json: User.find(params[:user_id]).items.create!(item_params), include: :user, status: :created
+    else
+      
+      render json: Item.create!(item_params), include: :user, status: :created
+    end
+  end
+
+
+  private
+  def item_params
+    params.permit(:name,:description,:price)
+  end
+
+  def no_record_error
+    render json: {error: "item not found"}, status: :not_found
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 
+
+  rescue_from ActiveRecord::RecordNotFound, with: :no_record_error
+
   def show
-    user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    user = User.find(params[:id])
+    render json: user, include: :items, status: :accepted
   end
 
+  private
+  def no_record_error
+    render json: {error: "user not found"}, status: :not_found
+  end
 end
